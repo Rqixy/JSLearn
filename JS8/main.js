335faaaa07fd8e233fe94e3c0d1b9cdaa2a3aa65
DESKTOP-SF66CJ8\yuuki <Rqixy@github.com>
2021-12-11 08:03:56 +0900

    submit JS8

--- JS8/main.js
+++ JS8/main.js
@@ -245,3 +245,49 @@
 
 // const sum2 = arr.reduce((a, x) => a +=x, 0);
 // console.log(sum2);
+
+// const words = ["Beachball", "Rodeo", "Angel", "Aardvark", "Xylophone", "November", "Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bail"];
+
+// const alphabetical = words.reduce((a, x) => {
+//     if(!a[x[0]])
+//         a[x[0]] = [];
+//     a[x[0]].push(x);
+//     console.log(a[x[0]]);
+//     return a;
+// }, {});
+
+// console.log(alphabetical);
+
+// const data = [3.3, 5, 7.2, 12, 4, 6, 10.3];
+// const stats = data.reduce((a, x) => {
+//     a.N++;
+//     let delta = x - a.mean;
+//     a.mean += delta / a.N;
+//     a.M2 += delta * (x - a.mean);
+//     return a;
+// }, { N: 0, mean: 0, M2: 0});
+
+// if(stats.N > 2) {
+//     stats.variance = stats.M2 / (stats.N - 1);
+//     stats.stdev = Math.sqrt(stats.variance);
+// }
+// console.log(stats);
+
+// const words = ["Beachball", "Rodeo", "Angel", "Aardvark", "Xylophone", "November", "Chocolate", "Papaya", "Uniform", "Joker", "Clover", "Bail"];
+
+// const longWords = words.reduce((a, w) => w.length > 6 ? a + " " + w : a, "").trim();
+// console.log(longWords);
+
+// const arr = [1, null, "hello", "world", true, undefined];
+// delete arr[3];
+// let result = arr.join();
+// console.log(result);
+
+// result = arr.join('');
+// console.log(result);
+// result = arr.join(' -- ');
+// console.log(result);
+
+// const Otomo = ["bird", "dog", "monkey"];
+// const html = '<ul><li>' + Otomo.join('</li><li>') + '</li></ul>';
+// console.log(html);
