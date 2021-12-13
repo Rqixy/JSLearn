3598a3188799b184b073aeefcc878c9965557062
DESKTOP-SF66CJ8\yuuki <Rqixy@github.com>
2021-12-13 09:44:05 +0900

    submit JS9

--- JS9/example.js
+++ JS9/example.js
@@ -1,43 +1,29 @@
-const Car = (function() {
-    const carProps = new WeakMap();
-
-    class Car {
-        constructor(make, model) {
-            this.make = make;
-            this.model = model;
-            this._userGears = ['P', 'N', 'R', 'D'];
-            carProps.set(this, { userGear: this._userGears[0] });
-        }
-
-        get userGear() { return carProps.get(this).userGear; }
-        set userGear(value) {
-            if(this._userGears.indexOf(value) < 0)
-                throw new Error(`ギア指定が正しくない：${value}`);
-            carProps.get(this).userGear = value;
-        }
-
-        shift(gear) { this.userGear = gear; }
-    } // class Car
-
-    return Car;
-})();
-
 // #@@range_begin(list1)
-// クラスの定義はexample/ch09/ex09-02-4/main.jsと同じ。
-const car1 = new Car();
-const car2 = new Car();
-console.log(car1.shift === Car.prototype.shift); // true
-console.log(car1.shift === car2.shift); // true
-car1.shift('D');
-// car1.shift('d'); // ここでこれを実行するとエラーになってしまう
-console.log(car1.userGear); // D
+class Car {
+    constructor() {
+    }
+}
 
-// 新たにメソッドshiftを定義。小文字で指定されても大文字に変換する
-car1.shift = function(gear) { this.userGear = gear.toUpperCase(); }
-console.log(car1.shift === Car.prototype.shift); // false
-console.log(car1.shift === car2.shift); // false
-car1.shift('d'); // ここで実行してもエラーにはならない
-console.log(car1.userGear); // D
+class InsurancePolicy { /* 保険契約 */
+}
 
-// car2.shift('d'); // これを実行するとエラーになってしまう
+function makeInsurable(o) {
+    o.addInsurancePolicy = function(p) { this.insurancePolicy = p; }
+    o.getInsurancePolicy = function() { return this.insurancePolicy; }
+    o.isInsured = function() { return !!this.insurancePolicy; }
+}
 // #@@range_end(list1)
+
+// #@@range_begin(list2)
+const car1 = new Car();
+makeInsurable(car1);
+console.log(car1.isInsured()); // false
+car1.addInsurancePolicy(new InsurancePolicy()); /* うまく行く */
+console.log(car1.isInsured()); // true
+// #@@range_end(list2)
+
+const car2 = new Car();
+makeInsurable(car2);
+console.log(car2.isInsured()); // false
+car2.addInsurancePolicy(new InsurancePolicy());
+console.log(car2.isInsured()); // true
--- JS9/main.js
+++ JS9/main.js
@@ -92,30 +92,30 @@
 
 // console.log(car1.userGear);
 
-const Car = (function() {
-    const carProps = new WeakMap();
-
-    class Car {
-        constructor(make, model) {
-            this.make = make;
-            this.model = model;
-            this._userGears = ['P', 'N', 'R', 'D'];
-            carProps.set(this, { userGear: this._userGears[0] });
-        }
-
-        get userGear() { return carProps.get(this).userGear; }
-        set userGear(value) {
-            if(this._userGears.indexOf(value) < 0) {
-                throw new Error(`ギア指定が正しくない : ${value}`);
-            }
-            carProps.get(this).userGear = value;
-        }
-
-        shift(gear) { this.userGear = gear; }
-    }
-
-    return Car;
-})();
+// const Car = (function() {
+//     const carProps = new WeakMap();
+//
+//     class Car {
+//         constructor(make, model) {
+//             this.make = make;
+//             this.model = model;
+//             this._userGears = ['P', 'N', 'R', 'D'];
+//             carProps.set(this, { userGear: this._userGears[0] });
+//         }
+//
+//         get userGear() { return carProps.get(this).userGear; }
+//         set userGear(value) {
+//             if(this._userGears.indexOf(value) < 0) {
+//                 throw new Error(`ギア指定が正しくない : ${value}`);
+//             }
+//             carProps.get(this).userGear = value;
+//         }
+//
+//         shift(gear) { this.userGear = gear; }
+//     }
+//
+//     return Car;
+// })();
 //
 // const car1 = new Car("Tesla", "Model S");
 // const car2 = new Car("Mazda", "3i");
@@ -165,16 +165,164 @@ const Car = (function() {
 // console.log(typeof EsCar);
 
 
-const car1 = new Car();
-const car2 = new Car();
-console.log(car1.shift === Car.prototype.shift);
-console.log(car1.shift === car2.shift);
-car1.shift('D');
+// const car1 = new Car();
+// const car2 = new Car();
+// console.log(car1.shift === Car.prototype.shift);
+// console.log(car1.shift === car2.shift);
+// car1.shift('D');
+// // car1.shift('d');
+// console.log(car1.userGear);
+//
+// car1.shift = function(gear) { this.userGear = gear.toUpperCase(); }
+// console.log(car1.shift === Car.prototype.shift);
+// console.log(car1.shift === car2.shift);
 // car1.shift('d');
-console.log(car1.userGear);
+// console.log(car1.userGear);
+// class Car {
+//     toString() {
+//         return `${this.make} ${this.model}: ${this.vin}`;
+//     }
+//     static getNextVin() {
+//         return Car.nextVin++;
+//     }
+//     constructor(make, model) {
+//         this.make = make;
+//         this.model = model;
+//         this.vin = Car.getNextVin();
+//     }
+//     static areSimilar(car1, car2) {
+//         return car1.make === car2.make && car1.model === car2.model;
+//     }
+//     static areSame(car1, car2) {
+//         return car1.vin === car2.vin;
+//     }
+// }
+// Car.nextVin = 0;
 
-car1.shift = function(gear) { this.userGear = gear.toUpperCase(); }
-console.log(car1.shift === Car.prototype.shift);
-console.log(car1.shift === car2.shift);
-car1.shift('d');
-console.log(car1.userGear);
+// const car1 = new Car("Tesla", "Model S");
+// const car2 = new Car("Mazda", "3i");
+// const car3 = new Car("Mazda", "3i");
+//
+// console.log(car1.vin);
+// console.log(car2.vin);
+// console.log(car3.vin);
+//
+//
+// console.log(Car.areSimilar(car1, car2));
+// console.log(Car.areSimilar(car2, car3));
+// console.log(Car.areSame(car2, car3));
+// console.log(Car.areSame(car2, car2));
+
+// class Vehicle {
+//     constructor()  {
+//         this.passengers = [];
+//         console.log("Vehicleが生成された");
+//     }
+//     addPassenger(p) {
+//         this.passengers.push(p);
+//     }
+// }
+//
+// class Car extends Vehicle {
+//     constructor() {
+//         super();
+//         console.log("Carが生成された");
+//     }
+//     deployAirbags() {
+//         console.log("バーン！");
+//     }
+// }
+
+// const v = new Vehicle();
+// v.addPassenger("太郎");
+// v.addPassenger("花子");
+// console.log(v.passengers);
+//
+// const c = new Car();
+// c.addPassenger("恵子");
+// c.addPassenger("緑");
+// console.log(c.passengers);
+// c.deployAirbags();
+
+// class Motorcycle extends Vehicle {}
+//
+// const c = new Car();
+// const m = new Motorcycle();
+// console.log(c instanceof Car);
+// console.log(c instanceof Vehicle);
+// console.log(m instanceof Motorcycle);
+// console.log(m instanceof Vehicle);
+
+// class Super {
+//     constructor() {
+//         this.name = 'Super';
+//         this.isSuper = true;
+//     }
+// }
+//
+// Super.prototype.sneaky = '非推奨！';
+//
+// class Sub extends Super {
+//     constructor() {
+//         super();
+//         this.name = 'Sub';
+//         this.isSub = true;
+//     }
+// }
+//
+// const obj = new Sub();
+//
+// for(let p in obj) {
+//     console.log(`${p}: ${obj[p]}` + (obj.hasOwnProperty(p) ? '' : ' (継承)'));
+// }
+
+// const car1 = new Car("Tesla", "Model S");
+// console.log(car1.toString());
+
+class Car {
+    constructor() {
+    }
+}
+
+class InsurancePolicy {
+}
+//
+// function makeInsurable(o) {
+//     o.addInsurancePolicy = function(p) { this.insurancePolicy = p;}
+//     o.getInsurancePolicy = function() { return this.insurancePolicy; }
+//     o.isInsured = function() { return !!this.insurancePolicy; }
+// }
+//
+// makeInsurable(Car.prototype);
+// const car1 = new Car();
+// makeInsurable(car1);
+// console.log(car1.isInsured());
+// car1.addInsurancePolicy(new InsurancePolicy());
+// console.log(car1.isInsured());
+//
+// const car2 = new Car();
+// console.log(car2.isInsured());
+// car2.addInsurancePolicy(new InsurancePolicy());
+// console.log(car2.isInsured());
+
+const ADD_POLICY = Symbol();
+const GET_POLICY = Symbol();
+const IS_INSURED = Symbol();
+const _POLICY = Symbol();
+function makeInsurable(o) {
+    o[ADD_POLICY] = function(p) {this[_POLICY] = p;}
+    o[GET_POLICY] = function() {return this[_POLICY]; }
+    o[IS_INSURED] = function() {return !!this[_POLICY]; }
+}
+
+makeInsurable(Car.prototype);
+
+const car1 = new Car();
+console.log(car1[IS_INSURED]());
+car1[ADD_POLICY](new InsurancePolicy());
+console.log(car1[IS_INSURED]());
+
+const car2 = new Car();
+console.log(car2[IS_INSURED]());
+car2[ADD_POLICY](new InsurancePolicy());
+console.log(car2[IS_INSURED]());
