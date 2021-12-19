a032c032ebd2df068d4badfb0b2e827e981f5b57
Rqixy <integral.ys1013@gmail.com>
2021-12-19 19:51:25 +0900

    JS13 途中

--- JS13/main.js
+++ JS13/main.js
@@ -90,9 +90,52 @@
 //     document.querySelector('.rainbow').style['background-color'] = rainbowIterator.next().value;
 // }, 1000);
 
-const v = function() { };
-const a = [1, 2, 3];
+// const v = function() { };
+// const a = [1, 2, 3];
 
-console.log(typeof(v));
-console.log(typeof(a));
-console.log(v instanceof Object);
+// console.log(typeof(v));
+// console.log(typeof(a));
+// console.log(v instanceof Object);
+
+// setTimeout(function() { console.log("hello"); }, 1500);
+
+// var i;
+// for(i = 5; i >= 0; i--) {
+//     setTimeout(function() {
+//         console.log(i === 0 ? "go!" : i);
+//     }, (5 - i) * 1000);
+// }
+
+// function loopBody(i) {
+//     setTimeout(function() {
+//         console.log(i === 0 ? "Go!" : i);
+//     }, (5 - i) * 1000);
+// }
+
+// var i;
+// for(i = 5; i >= 0; i--) {
+//     loopBody(i);
+// }
+
+// var i;
+// for(i = 5; i >= 0; i--) {
+//     (function(i) {
+//         setTimeout(function() {
+//             console.log(i === 0 ? "Go!" : i);
+//         }, (5 - i) * 1000);
+//     }) (i);
+// }
+
+// for(let i = 5; i >= 0; i--) {
+//     setTimeout(function() {
+//         console.log(i === 0 ? "Go!" : i);
+//     }, (5 - i) * 1000);
+// }
+
+// let i;
+// for(i = 5; i >= 0; i--) {
+//     setTimeout(function() {
+//         console.log(i === 0 ? "Go!" : i);
+//     }, (5 - i) * 1000);
+// }
+
