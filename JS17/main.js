06d55c5cea35eae675f0d2806eef289196a76a41
DESKTOP-SF66CJ8\yuuki <Rqixy@github.com>
2021-12-28 08:13:31 +0900

    add js17

---
+++ JS17/main.js
@@ -0,0 +1,5 @@
+const input = "As I was going to Saint Ives";
+console.log(input.startsWith("As"));
+console.log(input.endsWith("Ives"));
+console.log(input.startsWith("going", 9));
+console.log(input.endsWith("going", 14));
