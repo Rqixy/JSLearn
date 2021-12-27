64545c7ac4ff120b9a38ad20e583a324c3bcd042
Rqixy <integral.ys1013@gmail.com>
2021-12-27 21:52:55 +0900

    submit JS16

---
+++ JS16/main.js
@@ -0,0 +1,38 @@
+// const x = 19.51;
+// console.log(x.toFixed(3));
+// console.log(x.toFixed(2));
+// console.log(x.toFixed(1));
+// console.log(x.toFixed(0));
+
+// const x = 3800.5;
+// console.log(x.toExponential(4));
+// console.log(x.toExponential(3));
+// console.log(x.toExponential(2));
+// console.log(x.toExponential(1));
+// console.log(x.toExponential(0));
+
+// let x = 1000;
+// console.log(x.toPrecision(5));
+// console.log(x.toPrecision(4));
+// console.log(x.toPrecision(3));
+// console.log(x.toPrecision(2));
+// console.log(x.toPrecision(1));
+
+// x = 15.335;
+// console.log(x.toPrecision(6));
+// console.log(x.toPrecision(5));
+// console.log(x.toPrecision(4));
+// console.log(x.toPrecision(3));
+// console.log(x.toPrecision(2));
+// console.log(x.toPrecision(1));
+
+// const x = 12;
+// console.log(x.toString());
+// console.log(x.toString(10));
+// console.log(x.toString(16));
+// console.log(x.toString(8));
+// console.log(x.toString(2));
+
+// console.log(Math.E);
+
+// console.log(Math.random());
